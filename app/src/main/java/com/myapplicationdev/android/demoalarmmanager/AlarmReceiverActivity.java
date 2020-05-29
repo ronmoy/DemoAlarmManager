971f7cb3924fcd12e45b29ad8806d33e91e657b3
18016624 <18016624@myrp.edu.sg>
2020-05-29 10:54:19 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoalarmmanager/AlarmReceiverActivity.java
@@ -0,0 +1,13 @@
+package com.myapplicationdev.android.demoalarmmanager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmReceiverActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+    }
+}
